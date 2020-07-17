c2f0450141e0fa66759d7b8f37b1b720b578367b
sircac <sircac@gmail.com>
2020-07-17 22:59:22 +0200

    Auto-clean

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
 echo " > Open vim and download the bundles... "
 vim ~/.vimrc
-
+rm -fr ~/.vim/setup.sh ~/.vim/.git
