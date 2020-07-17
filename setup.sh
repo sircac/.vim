e09657dd4a1a6876113164dbdcaeb042f669da6d
sircac <sircac@gmail.com>
2020-07-17 23:05:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 mkdir ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
-echo " > Open vim and download the bundles... "
-vim ~/.vimrc
-rm -fr ~/.vim/setup.sh ~/.vim/.git
+echo -e "\n Just download all the bundles... \n" | tee ~/.vim/tmp.txt
+vim ~/.vim/tmp.txt
+rm -fr ~/.vim/setup.sh ~/.vim/.git ~/.vim/tmp.txt
