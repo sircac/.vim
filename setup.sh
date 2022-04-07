332df6a330b03e3eb621db290ad44fdbb1f85b67
sircac <sircac@gmail.com>
2022-04-07 12:22:50 +0200

    Removing alse the cheat sheet image

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
 echo -e "\n Just download all the bundles... \n" | tee ~/.vim/tmp.txt
 vim ~/.vim/tmp.txt
-rm -fr ~/.vim/setup.sh ~/.vim/.git ~/.vim/tmp.txt
+rm -fr ~/.vim/setup.sh ~/.vim/.git ~/.vim/tmp.txt ~/.vim/myvim.JPG
